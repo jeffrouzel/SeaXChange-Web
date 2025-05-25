143eb92208d70ee7630a15088e06a496ebc3e79d
Jeff Rouzel Bat-og <jabatog@up.edu.ph>
2025-05-25 23:29:46 +0800

    Grouped Routing Pages
    - protected auth for requirement of auth when accessing the routing pages

---
+++ Frontend/app/(protected)/layout.tsx
@@ -0,0 +1,9 @@
+import AuthWrapper from "@/components/AuthWrapper";
+
+export default function ProtectedLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <AuthWrapper>{children}</AuthWrapper>;
+}
