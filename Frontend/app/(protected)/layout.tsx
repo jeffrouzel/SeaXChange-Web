7caf0ac35fe0d52cae8f39d5b51fe6a780ef0332
Jeff Rouzel Bat-og <jabatog@up.edu.ph>
2025-05-29 08:44:09 +0800

    Update layout.tsx

--- Frontend/app/(protected)/layout.tsx
+++ Frontend/app/(protected)/layout.tsx
@@ -1,34 +1,9 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import AuthWrapper from "@/components/AuthWrapper";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-export const metadata: Metadata = {
-  title: "SeaXChange",
-  description: "A Blockchain Driven App for Tuna Supply Chain Management",
-};
-
-export default function RootLayout({
+export default function ProtectedLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
-  );
+}) {
+  return <AuthWrapper>{children}</AuthWrapper>;
 }
